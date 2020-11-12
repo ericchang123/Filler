58f69ae50417ae8fbf79032c16e816c6d7f5d85d
ericchang123 <eric.chang036@gmail.com>
2020-11-11 20:29:03 -0800

    Initial main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import Tkinter as tk
+
+class Filler(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+
+if __name__ == "__main__":
+    #game = Filler()
+    #game.title("Filler")
+    game.mainloop()
